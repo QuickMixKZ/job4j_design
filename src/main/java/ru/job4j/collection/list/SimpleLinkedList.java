70e670e4687991c9ee2df7d5780d8033c2a959d5
quickmixkz@gmail.com <QuickMixKZ>
2022-03-20 16:59:47 +0500

    2. Создать контейнер на базе связанного списка [#159]

--- src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -2,6 +2,7 @@ package ru.job4j.collection.list;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
 
@@ -12,12 +13,26 @@ public class SimpleLinkedList<E> implements List<E> {
 
     @Override
     public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
         size++;
+        modCount++;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        Objects.checkIndex(index, size);
+        Node<E> result = null;
+        for (int i = 0; i <= index; i++) {
+            result = result == null ? first : result.next;
+        }
+        return result.item;
     }
 
     @Override
@@ -26,6 +41,7 @@ public class SimpleLinkedList<E> implements List<E> {
 
             private final int expectedModCount = modCount;
             private int index;
+            private Node<E> currentNode;
 
             @Override
             public boolean hasNext() {
@@ -37,7 +53,13 @@ public class SimpleLinkedList<E> implements List<E> {
 
             @Override
             public E next() {
-                return null;
+                index++;
+                if (currentNode == null) {
+                    currentNode = first;
+                } else {
+                    currentNode = currentNode.next;
+                }
+                return currentNode.item;
             }
         };
     }
