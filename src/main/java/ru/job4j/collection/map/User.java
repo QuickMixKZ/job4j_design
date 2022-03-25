f4a740d4cc51ad122c3fbd32b2ef7b3cc05d10e9
quickmixkz@gmail.com <QuickMixKZ>
2022-03-25 17:40:55 +0500

    4. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -30,6 +30,13 @@ public class User {
 
     @Override
     public boolean equals(Object o) {
-        return this == o;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
     }
 }
