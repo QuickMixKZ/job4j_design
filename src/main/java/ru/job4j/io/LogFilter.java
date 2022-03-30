0f2f53194a413afe965381bcef6a24666a6104d3
quickmixkz@gmail.com <QuickMixKZ>
2022-03-30 17:06:25 +0500

    0.3. BufferedReader. [#252489 #282149]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -3,13 +3,15 @@ package ru.job4j.io;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class LogFilter {
     public List<String> filter(String file) {
         List<String> result = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                if (line.matches("^.* 404 [0-9-]*$")) {
+                Pattern pattern = Pattern.compile("^.* 404 [0-9-]*$");
+                if (pattern.matcher(line).matches()) {
                     result.add(line);
                 }
             }
@@ -35,7 +37,7 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
         save(log, "404.txt");
     }
 }
