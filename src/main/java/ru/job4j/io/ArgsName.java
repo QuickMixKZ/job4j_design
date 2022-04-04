1ee43f45e420da4998c86b758ea1a4ab74991d38
quickmixkz@gmail.com <QuickMixKZ>
2022-04-04 13:22:27 +0500

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException();
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        Pattern argsPattern = Pattern.compile(".*[^\\s]=[^\\s].*");
+        Pattern keyValuePattern = Pattern.compile("(\\w*?)=(.*)");
+        for (String arg : args) {
+            if (!argsPattern.matcher(arg).matches()) {
+                throw new IllegalArgumentException();
+            }
+            Matcher matcher = keyValuePattern.matcher(arg);
+            String key = null;
+            String value = null;
+            if (matcher.find()) {
+                key = matcher.group(1);
+                value = matcher.group(2);
+            }
+            values.put(key, value);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
