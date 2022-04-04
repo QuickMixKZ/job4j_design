ebefeea635420bee64c7cd29a1c56dba90a63233
quickmixkz@gmail.com <QuickMixKZ>
2022-04-04 21:21:47 +0500

    4.2. Поиск дубликатов [#315066 #284253]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,23 +1,21 @@
 package ru.job4j.io.duplicates;
 
-import org.w3c.dom.Node;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.BiConsumer;
 
 public class DuplicatesFinder {
+
+    private static void showDuplicates(Map<FileProperty, List<Path>> files) {
+        files.forEach((fileProperty, paths) -> paths.removeIf(path -> paths.size() < 2));
+        files.forEach((fileProperty, paths) -> paths.forEach(System.out::println));
+    }
+
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("./"), duplicatesVisitor);
-        Map<FileProperty, List<Path>> files = duplicatesVisitor.getDuplicates();
-        files.forEach((fileProperty, paths) -> paths.removeIf(path -> paths.size() < 2));
-        files.forEach((fileProperty, paths) -> paths.forEach(System.out::println));
+        showDuplicates(duplicatesVisitor.getDuplicates());
     }
 }
