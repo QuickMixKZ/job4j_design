ce19a28967b40745c2ff4c11b19050f1792d8224
quickmixkz@gmail.com <QuickMixKZ>
2022-04-05 13:18:45 +0500

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgsName argsName = ArgsName.of(args);
+        String directoryArg = argsName.get("d");
+        String excludeArg = argsName.get("e");
+        String outputArg = argsName.get("o");
+        File outputFile = new File(outputArg);
+        Path root = Paths.get(directoryArg);
+        if (!root.toFile().exists()) {
+            throw new IllegalArgumentException("Directory not found.");
+        }
+        List<Path> sources = Search.search(root, path -> !path.endsWith(excludeArg));
+        Zip zip = new Zip();
+        zip.packFiles(sources, outputFile);
+    }
+}
