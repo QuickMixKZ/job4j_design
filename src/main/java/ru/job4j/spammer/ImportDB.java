12086c9504bc792f227aae5505aaee896b4d79e5
QuickMixKZ <quickmixkz@gmail.com>
2022-05-04 13:32:52 +0500

    0.2. PrepareStatement [#379307 #296797]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -19,7 +19,7 @@ public class ImportDB {
         this.dump = dump;
     }
 
-    public List<User> load() throws IOException {
+    public List<User> load() {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
             rd.lines().forEach(s -> {
