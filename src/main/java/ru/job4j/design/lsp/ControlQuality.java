db1291fd70ce4d2db7eebe7e4f2f7edb016c9a8a
QuickMixKZ <quickmixkz@gmail.com>
2022-05-27 15:09:33 +0500

    1. Хранилище продуктов [#852 #305503]

--- src/main/java/ru/job4j/design/lsp/ControlQuality.java
+++ src/main/java/ru/job4j/design/lsp/ControlQuality.java
@@ -22,20 +22,4 @@ public class ControlQuality {
         this.storages = storages;
     }
 
-    public static void main(String[] args) {
-        List<Storage> storages = new ArrayList<>();
-        storages.add(new Warehouse());
-        storages.add(new Shop());
-        storages.add(new Trash());
-        ControlQuality controlQuality = new ControlQuality(storages);
-        List<Food> food = new ArrayList<>();
-        food.add(new Milk("Простоквашино", LocalDate.of(2022, 5, 1), LocalDate.of(2022, 1, 1), 500, 0));
-        food.add(new Bread("Простой хлеб", LocalDate.of(2030, 5, 1), LocalDate.of(2022, 5, 1), 40, 0));
-        food.add(new Water("Прозрачная вода", LocalDate.of(2022, 7, 1), LocalDate.of(2022, 5, 1), 100, 0));
-        food.add(new Water("Минеральная вода", LocalDate.of(2022, 6, 1), LocalDate.of(2022, 5, 1), 150, 0));
-        controlQuality.sortProducts(food);
-        storages.forEach(storage -> {
-            System.out.println(storage.getFoodList());
-        });
-    }
 }
