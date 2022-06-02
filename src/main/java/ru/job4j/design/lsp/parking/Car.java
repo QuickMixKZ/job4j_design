c0d9630d1ccdcd6ca72f9f0fdfcd65e7a958929b
QuickMixKZ <quickmixkz@gmail.com>
2022-06-02 12:37:02 +0500

    2. Парковка машин [#853]

---
+++ src/main/java/ru/job4j/design/lsp/parking/Car.java
@@ -0,0 +1,7 @@
+package ru.job4j.design.lsp.parking;
+
+public interface Car {
+
+    boolean parkCar(Parking parking);
+
+}
---
+++ src/main/java/ru/job4j/design/lsp/parking/Parking.java
@@ -0,0 +1,7 @@
+package ru.job4j.design.lsp.parking;
+
+public interface Parking {
+
+    boolean isFreeSpace(Car car);
+
+}
