c6ab2e5b67a2637ae7206b79226441d1accb55d4
QuickMixKZ <quickmixkz@gmail.com>
2022-06-08 13:11:16 +0500

    2. SQL Select by company [#2760]

---
+++ sql_scripts/company/select.sql
@@ -0,0 +1,32 @@
+SELECT
+	name
+FROM
+	person
+WHERE
+	company_id != 5;
+
+SELECT
+	p.name AS person_name,
+	c.name AS company_name
+FROM
+	person AS p
+	LEFT JOIN company AS c ON p.company_id = c.id;
+
+SELECT
+	c.name AS company,
+	count(p.name) AS workers
+FROM
+	person as p
+	LEFT JOIN company AS c ON p.company_id = c.id
+GROUP BY
+	c.name
+HAVING
+	count(p.name) = (SELECT
+						count(name)
+					FROM
+						person
+					GROUP BY
+						company_id
+					ORDER BY
+						count(name) DESC
+					LIMIT 1);
